5559e06e602c9fe73a0c8ff7bbfe84392f7afa63
shova lama <uprety.uttam@gmail.com>
2021-04-22 21:50:36 +0200

    Add project files.

---
+++ KonsolProjekt/Codes/StructKlasse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolProjekt
+{
+    public struct StructKlasse
+    {
+        DateTime afslutDato;
+        public StructKlasse(DateTime dato)
+        {
+            afslutDato = dato;
+        }
+        public int BeregnAfslutDato()
+        {
+            var now = DateTime.Now;
+
+            var remainingTime = (afslutDato - now).Days;
+            return remainingTime;
+        }
+    }
+
+    // Record type Klasse
+    public record KontaktInfo(string fornavn, string efternavn, string telefon, DateTime fødselsdato, char køn)
+    {
+        
+    }
+}
