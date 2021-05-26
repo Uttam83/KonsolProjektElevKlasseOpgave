bd04d30db080810eeab1c3a5659cf0cc3a41707e
Uttam83 <uprety.uttam@gmail.com>
2021-05-26 10:24:21 +0200

    HSH

---
+++ KonsolProjekt/Codes/AgeCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopOPGAVE1.Codes
+{
+
+
+    struct AgeCounter
+    {
+
+        public int? Alder { get; set; }
+
+        public AgeCounter(DateTime yearofRelease)
+        {
+            Alder = null;
+            Alder = GetAge(yearofRelease);
+        }
+
+        private int? GetAge(DateTime yearofRelease)
+        {
+            return DateTime.Now.Year - yearofRelease.Year;
+        }
+    }
+
+
+
+}
--- KonsolProjekt/Codes/Program.cs
+++ KonsolProjekt/Codes/Program.cs
@@ -1,53 +1,17 @@
-﻿using System;
+﻿using oopOPGAVE1.Codes;
+using System;
 
-namespace KonsolProjekt
+namespace oopOPGAVE1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Initialization of H1ElevKlasse class
-            H1ElevKlasse elevKlasse = new H1ElevKlasse();
+            Bil bil = new("BP20200", "Mercedis");
+            Console.WriteLine($"Mærke: {bil.Mærke}, nummerplade nr. {bil.GetNrPlate()}");
 
-            //Call the method with the elevKlasse object
-            var elevInfo = elevKlasse.GetElevInfo();
-
-            Console.WriteLine("Klasse eksempel \n");
-            Console.WriteLine("-------------------------");
-
-
-            Console.WriteLine(
-                "Fornavn: "+ elevInfo.Fornavn +
-                "\n Efternavn: " + elevInfo.Efternavn +
-                "\n Telefon: " + elevInfo.Telefonnr +
-                "\n Anden H1 Fag: " + elevInfo.AlleH1Fag[1]+
-                "\n H1Afslutdato: " + elevInfo.H1Afslutdato
-                );
-
-            Console.WriteLine("\nStruct eksempel ");
-            Console.WriteLine("------------------------- \n");
-
-
-            //Initialize struct with a parameter
-            StructKlasse udregningStruct = new StructKlasse(elevInfo.H1Afslutdato);
-
-            //call method with the struct object
-            var datoTilbage = udregningStruct.BeregnAfslutDato();
-            Console.WriteLine("Education will be Finished in:  {0} days",datoTilbage);
-
-           
-
-
-            Console.WriteLine("\n Records eksempel ");
-            Console.WriteLine("------------------------- \n");
-            string navn = elevKlasse.GetElevKontaktInfo().name;
-            string tlf = elevKlasse.GetElevKontaktInfo().telefonNr;
-            Console.WriteLine($"{ navn}, { tlf}");
-
-
-
-            Console.WriteLine("\n\npress any key to exit");
-            Console.ReadLine();
+            bil = new("BP20200", "Mercedis", new DateTime(2012, 1, 1));
+            Console.WriteLine($"Mærke: {bil.Mærke}, nummerplade nr. {bil.GetNrPlate()}, bilens alder: {bil.Alder} år gammel.");
         }
     }
 }
