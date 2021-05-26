5b5c99bbf25b59d0777566cfea4b4f1da70c7d87
Uttam83 <uprety.uttam@gmail.com>
2021-05-26 10:52:32 +0200

    rettet

--- KonsolProjekt/Codes/Bil.cs
+++ KonsolProjekt/Codes/Bil.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace KonsolProjekt
+namespace oopOPGAVE1.Codes
 {
-    public class Bil
+    class Bil
     {
-        
+        #region Property Fields
+
+        private string _nummerpladeNr;
+
+        public string GetNrPlate()
+        {
+            return _nummerpladeNr;
+        }
+
+        public void SetNrPlade(string value)
+        {
+            _nummerpladeNr = value;
+        }
+        #endregion
+
+
+
+        public string Mærke { get; set; }
+
+        public int? Alder { get; set; }
+
+
+
+        public Bil(string numberplate, string brand)
+        {
+            SetNrPlade(numberplate);
+
+            Mærke = brand;
+
+        }
+
+        public Bil(string numberplate, string brand, DateTime yearofRelease)
+        {
+            SetNrPlade(numberplate);
+            Mærke = brand;
+
+            AgeCounter ac = new(yearofRelease);
+            Alder = ac.Alder;
+
+        }
 
     }
 }
