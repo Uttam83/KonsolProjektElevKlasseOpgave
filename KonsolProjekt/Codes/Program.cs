780a74fcf7fdc29175055bbcbb5581386f5c532d
Uttam83 <uprety.uttam@gmail.com>
2021-06-04 11:52:31 +0200

    TeacherEmail

--- KonsolProjekt/Codes/Program.cs
+++ KonsolProjekt/Codes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KonsolProjekt.FolderForTextFile;
+using System;
 
 namespace KonsolProjekt
 {
@@ -6,48 +7,62 @@ namespace KonsolProjekt
     {
         static void Main(string[] args)
         {
-            //Initialization of H1ElevKlasse class
-            H1ElevKlasse elevKlasse = new H1ElevKlasse();
 
-            //Call the method with the elevKlasse object
-            var elevInfo = elevKlasse.GetElevInfo();
+            
+            Contacts con = new Contacts();
+          
 
-            Console.WriteLine("Klasse eksempel \n");
-            Console.WriteLine("-------------------------");
-
-
-            Console.WriteLine(
-                "Fornavn: "+ elevInfo.Fornavn +
-                "\n Efternavn: " + elevInfo.Efternavn +
-                "\n Telefon: " + elevInfo.Telefonnr +
-                "\n Anden H1 Fag: " + elevInfo.AlleH1Fag[1]+
-                "\n H1Afslutdato: " + elevInfo.H1Afslutdato
-                );
-
-            Console.WriteLine("\nStruct eksempel ");
-            Console.WriteLine("------------------------- \n");
-
-
-            //Initialize struct with a parameter
-            StructKlasse udregningStruct = new StructKlasse(elevInfo.H1Afslutdato);
-
-            //call method with the struct object
-            var datoTilbage = udregningStruct.BeregnAfslutDato();
-            Console.WriteLine("Education will be Finished in:  {0} days",datoTilbage);
+            System.Console.WriteLine(con.teachermail);
 
            
 
 
-            Console.WriteLine("\n Records eksempel ");
-            Console.WriteLine("------------------------- \n");
-            string navn = elevKlasse.GetElevKontaktInfo().name;
-            string tlf = elevKlasse.GetElevKontaktInfo().telefonNr;
-            Console.WriteLine("Name: {0} \nTelephoneno: {1}", navn, tlf);
 
 
 
-            Console.WriteLine("\n\npress any key to exit");
-            Console.ReadLine();
+
+            ////Initialization of H1ElevKlasse class
+            //H1ElevKlasse elevKlasse = new H1ElevKlasse();
+
+            ////Call the method with the elevKlasse object
+            //var elevInfo = elevKlasse.GetElevInfo();
+
+            //Console.WriteLine("Klasse eksempel \n");
+            //Console.WriteLine("-------------------------");
+
+
+            //Console.WriteLine(
+            //    "Fornavn: "+ elevInfo.Fornavn +
+            //    "\n Efternavn: " + elevInfo.Efternavn +
+            //    "\n Telefon: " + elevInfo.Telefonnr +
+            //    "\n Anden H1 Fag: " + elevInfo.AlleH1Fag[1]+
+            //    "\n H1Afslutdato: " + elevInfo.H1Afslutdato
+            //    );
+
+            //Console.WriteLine("\nStruct eksempel ");
+            //Console.WriteLine("------------------------- \n");
+
+
+            ////Initialize struct with a parameter
+            //StructKlasse udregningStruct = new StructKlasse(elevInfo.H1Afslutdato);
+
+            ////call method with the struct object
+            //var datoTilbage = udregningStruct.BeregnAfslutDato();
+            //Console.WriteLine("Education will be Finished in:  {0} days",datoTilbage);
+
+
+
+
+            //Console.WriteLine("\n Records eksempel ");
+            //Console.WriteLine("------------------------- \n");
+            //string navn = elevKlasse.GetElevKontaktInfo().name;
+            //string tlf = elevKlasse.GetElevKontaktInfo().telefonNr;
+            //Console.WriteLine("Name: {0} \nTelephoneno: {1}", navn, tlf);
+
+
+
+            //Console.WriteLine("\n\npress any key to exit");
+            //Console.ReadLine();
         }
     }
 }
---
+++ KonsolProjekt/FolderForTextFile/Contacts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KonsolProjekt.FolderForTextFile
+{
+    class Contacts
+    {
+        private string _teacherEmail;
+
+        public string teachermail
+        {
+            get
+            {
+
+            try
+                {
+                    if (_teacherEmail == null)
+                    {
+                        string envirnt = Environment.CurrentDirectory + @"\Files\Contacts.txt";
+                        _teacherEmail = System.IO.File.ReadAllText(envirnt);
+                    }
+                }
+                catch (Exception exc)
+                    {
+                        _teacherEmail = exc.Message;
+
+                    }
+                    return _teacherEmail;
+               
+            }
+        }
+
+        
+    }
+
+        
+}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+       
+       
+       
+    
+
